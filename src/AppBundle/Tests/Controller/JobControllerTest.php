d16acdff8a72e84cfeea95ec616d2db6f6309c60
Dragos Holban <dragos@intelligentbee.com>
2017-07-13 10:06:22 +0300

    Day 9

--- src/AppBundle/Tests/Controller/JobControllerTest.php
+++ src/AppBundle/Tests/Controller/JobControllerTest.php
@@ -3,53 +3,74 @@
 namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
+ 
 class JobControllerTest extends WebTestCase
 {
-    /*
-    public function testCompleteScenario()
+    public function getMostRecentProgrammingJob()
     {
-        // Create a new client to browse the application
-        $client = static::createClient();
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
 
-        // Create a new entry in the database
-        $crawler = $client->request('GET', '/job/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /job/");
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
-
-        // Fill in the form and submit it
-        $form = $crawler->selectButton('Create')->form(array(
-            'appbundle_job[field_name]'  => 'Test',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
-
-        // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
-
-        $form = $crawler->selectButton('Update')->form(array(
-            'appbundle_job[field_name]'  => 'Foo',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
-
-        // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
-        $crawler = $client->followRedirect();
-
-        // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+        $query = $em->createQuery('SELECT j from AppBundle:Job j LEFT JOIN j.category c WHERE c.slug = :slug AND j.expiresAt > :date ORDER BY j.createdAt DESC');
+        $query->setParameter('slug', 'programming');
+        $query->setParameter('date', date('Y-m-d H:i:s', time()));
+        $query->setMaxResults(1);
+        
+        return $query->getSingleResult();
     }
 
-    */
+    public function getExpiredJob()
+    {
+      $kernel = static::createKernel();
+      $kernel->boot();
+      $em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+
+      $query = $em->createQuery('SELECT j from AppBundle:Job j WHERE j.expiresAt < :date');     $query->setParameter('date', date('Y-m-d H:i:s', time()));
+      $query->setMaxResults(1);
+      return $query->getSingleResult();
+    }
+
+    public function testIndex()
+    {
+      // get the custom parameters from app config.yml
+      $kernel = static::createKernel();
+      $kernel->boot();
+      $max_jobs_on_homepage = $kernel->getContainer()->getParameter('max_jobs_on_homepage');
+      $max_jobs_on_category = $kernel->getContainer()->getParameter('max_jobs_on_category');
+
+      $client = static::createClient();
+
+      $crawler = $client->request('GET', '/');
+      $this->assertEquals('AppBundle\Controller\JobController::indexAction', $client->getRequest()->attributes->get('_controller'));
+
+      // expired jobs are not listed
+      $this->assertTrue($crawler->filter('.jobs td.position:contains("Expired")')->count() == 0);
+
+      // only $max_jobs_on_homepage jobs are listed for a category
+      $this->assertTrue($crawler->filter('.category_programming tr')->count() <= $max_jobs_on_homepage);
+      $this->assertTrue($crawler->filter('.category_design .more_jobs')->count() == 0);
+      $this->assertTrue($crawler->filter('.category_programming .more_jobs')->count() == 1);
+
+      // jobs are sorted by date
+      $this->assertTrue($crawler->filter('.category_programming tr')->first()->filter(sprintf('a[href*="/%d/"]', $this->getMostRecentProgrammingJob()->getId()))->count() == 1);
+
+      // each job on the homepage is clickable and give detailed information
+      $job = $this->getMostRecentProgrammingJob();
+      $link = $crawler->selectLink('Web Developer')->first()->link();
+      $crawler = $client->click($link);
+      $this->assertEquals('AppBundle\Controller\JobController::showAction', $client->getRequest()->attributes->get('_controller'));
+      $this->assertEquals($job->getCompanySlug(), $client->getRequest()->attributes->get('company'));
+      $this->assertEquals($job->getLocationSlug(), $client->getRequest()->attributes->get('location'));
+      $this->assertEquals($job->getPositionSlug(), $client->getRequest()->attributes->get('position'));
+      $this->assertEquals($job->getId(), $client->getRequest()->attributes->get('id'));
+
+      // a non-existent job forwards the user to a 404
+      $crawler = $client->request('GET', '/job/foo-inc/milano-italy/0/painter');
+      $this->assertTrue(404 === $client->getResponse()->getStatusCode());
+
+      // an expired job page forwards the user to a 404
+      $crawler = $client->request('GET', sprintf('/job/sensio-labs/paris-france/%d/web-developer', $this->getExpiredJob()->getId()));
+      $this->assertTrue(404 === $client->getResponse()->getStatusCode());
+    }
 }
